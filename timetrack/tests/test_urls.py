34666ae8bea4979a46967bb5583f964fb48bda89
Sam Ireland <sam.ireland.uk@gmail.com>
2017-09-04 22:17:33 +0100

    Start 0.1 fresh

--- timetrack/tests/test_urls.py
+++
@@ -1,7 +0,0 @@
-from pontefract.tests import UrlTest
-from timetrack import views
-
-class UsersUrlTests(UrlTest):
-
-    def test_root_url_resolves_to_home_page(self):
-        self.check_url_returns_view("/", views.home_page)
--- timetrack/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from users.views import signup_page
-
-# Create your views here.
-def home_page(request):
-    if not request.user.is_authenticated:
-        return signup_page(request)
-    return render(request, "home.html")
