9d9fbf4ff751d0ff6390b0f8c7ac4db38230d6d7
Sam Ireland <sam.ireland.uk@gmail.com>
2018-01-29 19:56:26 +0000

    Add databases and apps/middleware

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,24 @@
 import os
-from .secrets import SECRET_KEY
+from .secrets import BASE_DIR, SECRET_KEY, DATABASES
 
 ALLOWED_HOSTS = []
 DEBUG = True
 
 ROOT_URLCONF = "core.urls"
+
+INSTALLED_APPS = [
+ "django.contrib.contenttypes",
+ "django.contrib.staticfiles",
+ "django.contrib.auth",
+ "django.contrib.sessions",
+ "core"
+]
+
+MIDDLEWARE = [
+ "django.contrib.sessions.middleware.SessionMiddleware",
+ "django.middleware.common.CommonMiddleware",
+ "django.middleware.csrf.CsrfViewMiddleware",
+ "django.contrib.auth.middleware.AuthenticationMiddleware",
+]
+
+STATIC_URL = "/static/"
