de3a6cd0e7c78e8443ce232ffc6d932d1ab826cf
Sam Ireland <sam.ireland.uk@gmail.com>
2018-02-19 22:45:02 +0000

    Restart 0.1 *again*

--- core/tests/test_urls.py
+++
@@ -1,7 +0,0 @@
-from testarsenal import DjangoTest
-import core.views as views
-
-class UrlTests(DjangoTest):
-
-    def test_root_url(self):
-        self.check_url_returns_view("/", views.root)
--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-import core.views as views
-
-urlpatterns = [
- path(r"", views.root),
-]
