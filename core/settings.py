314860715176d88d9fed0124a4c431116d8c2d54
Sam Ireland <sam.ireland.uk@gmail.com>
2018-02-20 18:01:01 +0000

    First DB setup

--- core/settings.py
+++ core/settings.py
@@ -4,3 +4,20 @@ ALLOWED_HOSTS = []
 DEBUG = True
 
 ROOT_URLCONF = "core.urls"
+
+INSTALLED_APPS = [
+ "django.contrib.contenttypes",
+ "django.contrib.staticfiles",
+ "django.contrib.auth",
+ "django.contrib.sessions",
+ "core"
+]
+
+MIDDLEWARE = [
+ "django.contrib.sessions.middleware.SessionMiddleware",
+ "django.middleware.common.CommonMiddleware",
+ "django.middleware.csrf.CsrfViewMiddleware",
+ "django.contrib.auth.middleware.AuthenticationMiddleware",
+]
+
+STATIC_URL = "/static/"
