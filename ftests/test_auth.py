41e0c2c93ff28ce712f539c405e540c931dee20b
Sam Ireland <sam.ireland.uk@gmail.com>
2018-02-21 12:03:03 +0000

    Add site description

--- ftests/test_auth.py
+++ ftests/test_auth.py
@@ -9,3 +9,7 @@ class SignupTests(FunctionalTest):
         # The name is prominently displayed
         h1 = self.browser.find_element_by_tag_name("h1")
         self.assertEqual(h1.text, "pontefract")
+
+        # There is a brief description
+        description = self.browser.find_element_by_id("site-description")
+        self.assertIn("time tracking for individuals", description.text.lower())
