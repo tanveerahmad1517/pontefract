526e47d43294d39583d3cd49283c8eb778d8aa50
Sam Ireland <sam.ireland.uk@gmail.com>
2018-05-05 12:10:31 +0100

    Make projects sort by duration

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True)
     timezone = TimeZoneField(default="UTC")
-    
+
 
     def first_month(self):
         """What was the first month that the user has sessions for?"""
@@ -23,3 +23,7 @@ class User(AbstractUser):
             first = sessions.order_by("start")[0]
             start = first.local_start()
             return date(start.year, start.month, 1)
+
+
+    def projects_by_time(self):
+        return sorted(self.project_set.all(), key=lambda p: sum([s.duration() for s in p.session_set.all()]), reverse=True)
